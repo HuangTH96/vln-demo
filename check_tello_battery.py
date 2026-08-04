fb0e5d9ab352b9f5964145e88fa707cc06a136bf
ZhiLu <60755772+HuangTH96@users.noreply.github.com>
2026-08-04 13:45:50 +0800

    fix：移除PATH硬编码

--- check_tello_battery.py
+++ check_tello_battery.py
@@ -1,9 +1,9 @@
-# =====================================================================================
-# 仅windows - vs code需要配置路径
-# VS Code激活conda环境时只加了Python解释器的路径，没有完整初始化conda环境的所有PATH，需要手动配置
 import os
-os.environ["PATH"] = r"D:\anaconda\envs\tello\Library\bin" + ";" + os.environ["PATH"]
-# ======================================================================================
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__)))    # 项目源目录
+from vln_demo.utils import apply_windows_conda_path_fix
+apply_windows_conda_path_fix()
+
 from djitellopy import Tello
 
 tello = Tello()
